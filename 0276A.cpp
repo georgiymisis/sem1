fc681db11721e303b257e41d94038ff297837558
georgiymisis <121457085+georgiymisis@users.noreply.github.com>
2022-12-27 00:16:43 +0300

    Create 0276A.cpp

---
+++ 0276A.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+int main() {
+    int n(0),k(0),f1(0),t(0), z(0), s (0);
+    long int m(-10000000000);
+    std::cin >> n >> k;
+    while (n > s){
+        std::cin >> f1 >> t;
+        if (t>k){
+            z = f1 - t + k;
+        }
+        else {
+            z = f1;
+        }
+        if (z > m) {
+            m = z;
+        }
+        s = s + 1;
+    }
+    std::cout << (m);
+    return (0);
+}
